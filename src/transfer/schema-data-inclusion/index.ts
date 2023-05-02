bb6aa6eacd66990d92a2ce784835bde5c55daee2
Marc Gavanier <marc.gavanier@gmail.com>
2023-05-02 20:55:11 +0200

    fix: add missing exports to data inclusion service to structure

--- src/transfer/schema-data-inclusion/index.ts
+++ src/transfer/schema-data-inclusion/index.ts
@@ -1,3 +1,4 @@
 export * from './from-schema-data-inclusion';
+export * from './service-to-structure';
 export * from './to-schema-data-inclusion/to-schema-data-inclusion';
 export * from './schema-data-inclusion';
