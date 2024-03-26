124868be5244c79cf0831f8affb8d406c9ee4e8e
Marc Gavanier <marc.gavanier@gmail.com>
2024-03-26 16:41:05 +0100

    feat(schema)!: update frais à charge

--- src/models/condition-acces/errors/conditions-acces.error.ts
+++
@@ -1,9 +0,0 @@
-import { ModelError } from '../../../errors';
-import { ConditionAcces, ConditionsAccesIndefinie } from '../condition-acces';
-import { LieuMediationNumerique } from '../../lieu-mediation-numerique';
-
-export class ConditionsAccesError extends ModelError<LieuMediationNumerique> {
-  constructor(conditionsAcces: ConditionAcces | ConditionsAccesIndefinie) {
-    super('conditions_acces', `La condition d'accès '${conditionsAcces}' n'est pas une valeur admise`);
-  }
-}
---
+++ src/models/frais-a-charge/index.ts
@@ -0,0 +1,2 @@
+export * from './errors';
+export * from './frais-a-charge';
