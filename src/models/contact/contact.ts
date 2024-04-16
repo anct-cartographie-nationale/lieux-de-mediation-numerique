366364bd63a07fa76825f5bbe52b2c191e1048f2
Marc Gavanier <marc.gavanier@gmail.com>
2024-04-16 14:33:07 +0200

    refactor(schema): order calling codes

--- src/models/contact/contact.ts
+++ src/models/contact/contact.ts
@@ -17,7 +17,7 @@ export type Contact = Model<
 export type ContactToValidate = Omit<Contact, 'isContact'>;
 
 const TELEPHONE_REG_EXP: RegExp =
-  /^(?:(?:\+|00)(?:33|594|262|596|269|687|689|590|508|681)[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)(?:(?:[1-9](?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]\d{3}){2})|\d{6}|\s\d{3}(?:\s\d{2}){3})$/u;
+  /^(?:(?:\+|00)(?:33|262|269|508|590|594|596|681|687|689)[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)(?:(?:[1-9](?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]\d{3}){2})|\d{6}|\s\d{3}(?:\s\d{2}){3})$/u;
 
 export const isValidTelephone = (telephone: string): boolean => TELEPHONE_REG_EXP.test(telephone);
 
