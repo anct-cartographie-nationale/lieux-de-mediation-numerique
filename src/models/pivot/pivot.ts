1e844c7577c1b472b3cffccbf38d478a53a4a720
Marc Gavanier <marc.gavanier@protonmail.com>
2025-09-30 16:04:40 +0200

    feat: remove ridet as pivot (#65)

--- src/models/pivot/pivot.ts
+++ src/models/pivot/pivot.ts
@@ -1,13 +1,11 @@
 import { isSiret, Siret } from '../siret';
-import { isRidet, Ridet } from '../ridet';
 import { isRna, Rna } from '../rna';
 import { PivotError } from './errors';
 
-export type Pivot = Ridet | Rna | Siret;
+export type Pivot = Rna | Siret;
 
 const throwPivotError = (pivot: string): Pivot => {
   throw new PivotError(pivot);
 };
 
-export const Pivot = (pivot: string): Pivot =>
-  isSiret(pivot) || isRna(pivot) || isRidet(pivot) ? pivot : throwPivotError(pivot);
+export const Pivot = (pivot: string): Pivot => (isSiret(pivot) || isRna(pivot) ? pivot : throwPivotError(pivot));
--- src/models/ridet/errors/ridet.error.ts
+++
@@ -1,7 +0,0 @@
-import { ModelError } from '../../../errors';
-
-export class RidetError extends ModelError<{ ridet: string }> {
-  constructor(ridet: string) {
-    super('ridet', `Le Ridet ${ridet} n'est pas valide`);
-  }
-}
